c07acf6e2e0f3ca57be7dc8aba334312c4b1ce15
jaulingui <jaulingui@hotmail.com>
2025-09-28 13:19:29 -0300

    Método 'Agregar cliente'

--- negocio/ClienteNegocio.cs
+++ negocio/ClienteNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace negocio
 {
-    internal class ClienteNegocio
+    public class ClienteNegocio
     {
-    }
+        public void agregar(Cliente nuevo)
+        {            
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("INSERT INTO CLIENTES (Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP) VALUES (@Documento,@Nombre,@Apellido,@Email,@Direccion,@Ciudad,@CP)");
+                datos.setearParametro("@Documento", nuevo.Documento);
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Apellido", nuevo.Apellido);
+                datos.setearParametro("@Email", nuevo.Email);
+                datos.setearParametro("@Direccion", nuevo.Direccion);
+                datos.setearParametro("@Ciudad", nuevo.Ciudad);
+                datos.setearParametro("@CP", nuevo.CP);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }    
 }
