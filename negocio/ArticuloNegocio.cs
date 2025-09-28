a199389d25b15c587c94793ae1a69edb477266a9
jaulingui <jaulingui@hotmail.com>
2025-09-28 12:49:09 -0300

    Se vuelve a subir proyecto de dominio y negocio con sus referencias y dirección a Base de datos

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,178 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT A.Id, Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria,a.IdMarca,a.IdCategoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["descripcion"];
+                    aux.Precio = (decimal)datos.Lector["precio"];
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)datos.Lector["IdMarca"]; //Se agrega linea para traer el id.Marca (Modificar art)
+                    aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["IdCategoria"];//Se agrega linea para traer el id.Categoria(modificar art)
+                    aux.Categoria.Descripcion = (string)datos.Lector["Categoria"];
+
+                    lista.Add(aux);
+                }
+            return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+
+        }
+        public void agregar(Articulo nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+ 
+                datos.setearConsulta("INSERT INTO ARTICULOS (Codigo, Nombre, Descripcion, Precio, IdCategoria, IdMarca) " + "VALUES ('" + nuevo.Codigo + "', '" + nuevo.Nombre + "', '" + nuevo.Descripcion + "', " + nuevo.Precio + ", " + nuevo.Categoria.Id + ", " + nuevo.Marca.Id + ")");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminarRegistro(int id)
+        {
+            try
+            {
+                AccesoDatos datos = new AccesoDatos();
+                datos.setearConsulta("delete from ARTICULOS where id = @id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public List<Articulo> Filtrar(string campo, string criterio, int filtro)
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            string consulta = "SELECT A.Id, Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id AND ";
+
+            if (campo == "Id")
+            {
+                switch (criterio)
+                {
+                    case "Mayor a":
+                        consulta += "A.Id >" + filtro;
+                        break;
+                    case "Menor a":
+                        consulta += "A.Id <" + filtro;
+                        break;
+                    case "Igual a":
+                        consulta += "A.Id =" + filtro;
+                        break;
+                }
+            }
+            else
+            {
+                switch (criterio)
+                {
+                    case "Mayor a":
+                        consulta += "Precio > " + filtro;
+                        break;
+                    case "Menor a":
+                        consulta += "Precio < " + filtro;
+                        break;
+                    case "Igual a":
+                        consulta += "Precio = " + filtro;
+                        break;
+                }
+
+
+            }
+            datos.setearConsulta(consulta);
+            datos.ejecutarLectura();
+
+            while (datos.Lector.Read())
+            {
+                Articulo aux = new Articulo();
+                aux.Id = (int)datos.Lector["Id"];
+                aux.Codigo = (string)datos.Lector["Codigo"];
+                aux.Nombre = (string)datos.Lector["Nombre"];
+                aux.Descripcion = (string)datos.Lector["descripcion"];
+                aux.Precio = (decimal)datos.Lector["precio"];
+                aux.Marca = new Marca();
+                aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+                aux.Categoria = new Categoria();
+
+                aux.Categoria.Descripcion = (string)datos.Lector["Categoria"];
+
+                lista.Add(aux);
+            }
+            return lista;
+
+        }
+
+        public void Modificar(Articulo modificado)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                datos.setearConsulta("update ARTICULOS set Codigo = @codigo ,Nombre = @nombre, Descripcion = @descripcion, IdMarca = @idmarca, IdCategoria=@idcategoria, Precio= @precio Where id = @id");
+                datos.setearParametro("@codigo", modificado.Codigo);
+                datos.setearParametro("@nombre", modificado.Nombre);
+                datos.setearParametro("@descripcion", modificado.Descripcion);
+                datos.setearParametro("@idmarca", modificado.Marca.Id);
+                datos.setearParametro("@idcategoria", modificado.Categoria.Id);
+                datos.setearParametro("@precio", modificado.Precio);
+                datos.setearParametro("@id", modificado.Id);
+                
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+
+    }
+    }
+
---
+++ negocio/ImagenNegocio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Imagen> Listar()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id, IdArticulo, ImagenUrl from IMAGENES");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void agregar(Imagen nueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO IMAGENES (IdArticulo, ImagenUrl) VALUES (@idArticulo, @imagenUrl)");
+                datos.setearParametro("@idArticulo", nueva.IdArticulo);
+                datos.setearParametro("@imagenUrl", nueva.ImagenUrl);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void EliminarImagen(int id)
+        {
+            try
+            {
+                AccesoDatos datos = new AccesoDatos();
+                datos.setearConsulta("delete from IMAGENES where id = @id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }            
+        }
+
+        public void ModificarImagen(Imagen imagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("update IMAGENES set IdArticulo = @idArticulo, ImagenUrl = @imagenUrl where id = @id");
+                datos.setearParametro("@idArticulo", imagen.IdArticulo);
+                datos.setearParametro("@imagenUrl", imagen.ImagenUrl);
+                datos.setearParametro("@id", imagen.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+
+
+}
