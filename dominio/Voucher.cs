93aff3bc9a6d3acb122b92a950a464aee1b2c9a8
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-09-28 15:48:48 -0300

    Clase Voucher
    Se crea clase Voucher

---
+++ dominio/Voucher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Voucher
+    {
+        public int CodigoVoucher { get; set; }
+        public string IdCliente { get; set; }
+        public DateTime FechaCanje { get; set; }
+        public int IdArticulo { get; set; }
+    }
+}
