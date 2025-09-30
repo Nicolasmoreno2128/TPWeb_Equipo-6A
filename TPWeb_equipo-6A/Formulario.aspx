6e6e0bd2eac3cc3d2164518377d1400c3adb7bc8
julianfparodi <julianfacundoparodi@gmail.com>
2025-09-29 23:30:13 -0300

    Diseño de pagina de formulario

---
+++ TPWeb_equipo-6A/Formulario.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MiMaster.Master" AutoEventWireup="true" CodeBehind="Formulario.aspx.cs" Inherits="TPWeb_equipo_6A.WebForm3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h1 class="text-center mt-2">Ingresa tus datos</h1>
+    <div class="container mt-3" style="max-width: 1000px;">
+        <div class="row g-3">
+            <div class="col-sm-3">
+                <label for="txbDNI" class="form-label">DNI</label>
+                <asp:TextBox runat="server" ID="txbDNI" CssClass="form-control" placeholder="DNI" />
+            </div>
+            <div class="row g-3">
+                <div class="col">
+                    <label for="txbNombre" class="form-label">Nombre</label>
+                    <asp:TextBox runat="server" ID="txbNombre" CssClass="form-control" placeholder="Nombre" />
+                </div>
+                <div class="col">
+                    <label for="txbApellido" class="form-label">Apellido</label>
+                    <asp:TextBox runat="server" ID="txbApellido" CssClass="form-control" placeholder="Apellido" />
+                </div>
+                <div class="col">
+                    <label for="txbEmail" class="form-label">Email</label>
+                    <asp:TextBox type="email" runat="server" ID="TxbEmail" CssClass="form-control" placeholder="Email" />
+                </div>
+            </div>
+
+            <div class="row g-3">
+                <div class="col md-6">
+                    <label for="txbDireccion" class="form-label">Direccion</label>
+                    <asp:TextBox runat="server" ID="txbDireccion" CssClass="form-control" placeholder="Direccion" />
+                </div>
+                <div class="col md-3">
+                    <label for="txbCiudad" class="form-label">Ciudad</label>
+                    <asp:TextBox runat="server" ID="txbCiudad" CssClass="form-control" placeholder="Ciudad" />
+                </div>
+                <div class="col md-3">
+                    <label for="txbCodPostal" class="form-label">Codigo Postal</label>
+                    <asp:TextBox runat="server" ID="txbCodPostal" CssClass="form-control" placeholder="CP" />
+                </div>
+            </div>
+
+            <div class="form-check mt-3">
+                <asp:CheckBox ID="chbAcepto" CssClass="form-check-input" runat="server" Text="" />
+                <label class="form-check-label" for="chbAcepto">
+                    Acepto los terminos y condiciones.
+                </label>
+            </div>
+
+        </div>
+        <asp:Button Text="Participar!" ID="btnParticipar" CssClass="btn btn-primary mt-3" runat="server" />
+    </div>
+
+
+</asp:Content>
---
+++ TPWeb_equipo-6A/Formulario.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPWeb_equipo_6A
+{
+
+
+    public partial class WebForm3
+    {
+
+        /// <summary>
+        /// Control txbDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbDNI;
+
+        /// <summary>
+        /// Control txbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbNombre;
+
+        /// <summary>
+        /// Control txbApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbApellido;
+
+        /// <summary>
+        /// Control TxbEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxbEmail;
+
+        /// <summary>
+        /// Control txbDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbDireccion;
+
+        /// <summary>
+        /// Control txbCiudad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbCiudad;
+
+        /// <summary>
+        /// Control txbCodPostal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbCodPostal;
+
+        /// <summary>
+        /// Control chbAcepto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbAcepto;
+
+        /// <summary>
+        /// Control btnParticipar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnParticipar;
+    }
+}
